2173ebe4420b44bb076685d5d924ca5f040c8f1f
rafaelcparanhos <desenvolvimento@rafaelcparanhos.com.br>
2020-06-17 11:09:46 -0300

    Correção na navbar

--- public/js/website.js
+++ public/js/website.js
@@ -5,18 +5,20 @@ $(document).ready(function() {
 
     if (domain == '127.0.0.1')
         domain = domain + ':8000';
-
+    var deviceWidth = $( window ).width();
     var blackImgSource = 'http://' + domain + '/img/logos/logo-navbar.png';
     var whiteImgSource = 'http://' + domain + '/img/logos/logo-navbar-white.png';
 
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 19) {
-            $('.navbar-brand img').attr('src', blackImgSource);
-        }else {
-            $('.navbar-brand img').attr('src', whiteImgSource)
-        }
-    });
+    if (deviceWidth >= 975){
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            if (scroll >= 19) {
+                $('.navbar-brand img').attr('src', blackImgSource);
+            }else {
+                $('.navbar-brand img').attr('src', whiteImgSource)
+            }
+        });
+    }
 
     $('.phases-front').on('mouseenter',function(){
         $(this).trigger('click');
